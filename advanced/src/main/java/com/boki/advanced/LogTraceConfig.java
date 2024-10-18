0d65de9957ecd21d35ec451497a220c7ed8a83b2
boki <lsb530@naver.com>
2024-10-18 20:46:49 +0900

    ThreadLocal 동기화 - 적용

--- advanced/src/main/java/com/boki/advanced/LogTraceConfig.java
+++ advanced/src/main/java/com/boki/advanced/LogTraceConfig.java
@@ -1,7 +1,7 @@
 package com.boki.advanced;
 
-import com.boki.advanced.trace.logtrace.FieldLogTrace;
 import com.boki.advanced.trace.logtrace.LogTrace;
+import com.boki.advanced.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,7 +10,7 @@ public class LogTraceConfig {
 
     @Bean
     public LogTrace logTrace() {
-        return new FieldLogTrace();
+        return new ThreadLocalLogTrace();
     }
 
 }
