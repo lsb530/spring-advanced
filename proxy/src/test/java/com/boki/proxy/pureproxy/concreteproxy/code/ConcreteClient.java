8fee99238ca498b468328b7314535f02a6b351c2
boki <lsb530@naver.com>
2024-10-28 20:54:55 +0900

    구체 클래스 기반 프록시 - 예제2

--- proxy/src/test/java/com/boki/proxy/pureproxy/concreteproxy/code/ConcreteClient.java
+++ proxy/src/test/java/com/boki/proxy/pureproxy/concreteproxy/code/ConcreteClient.java
@@ -2,6 +2,7 @@ package com.boki.proxy.pureproxy.concreteproxy.code;
 
 public class ConcreteClient {
 
+    // ConcreteLogic, TimeProxy 모두 주입 가능
     private final ConcreteLogic concreteLogic;
 
     public ConcreteClient(ConcreteLogic concreteLogic) {
