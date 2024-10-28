d8e019f6bfa087401ac7fa87722174a368d8f400
boki <lsb530@naver.com>
2024-10-28 16:30:15 +0900

    예제 프로젝트 만들기 v2

--- proxy/src/main/java/com/boki/proxy/ProxyApplication.java
+++ proxy/src/main/java/com/boki/proxy/ProxyApplication.java
@@ -1,11 +1,12 @@
 package com.boki.proxy;
 
 import com.boki.proxy.config.AppV1Config;
+import com.boki.proxy.config.AppV2Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-@Import(AppV1Config.class)
+@Import({AppV1Config.class, AppV2Config.class})
 // config 패키지 하위의 Configuration들이 컴포넌트 스캔에 잡히지 않게 하기 위함
 @SpringBootApplication(scanBasePackages = "com.boki.proxy.app")
 public class ProxyApplication {
---
+++ proxy/src/main/java/com/boki/proxy/app/v2/OrderControllerV2.java
@@ -0,0 +1,31 @@
+package com.boki.proxy.app.v2;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequestMapping("/v2")
+@RestController
+public class OrderControllerV2 {
+
+    private final OrderServiceV2 orderService;
+
+    public OrderControllerV2(OrderServiceV2 orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping("/request")
+    public String request(@RequestParam("itemId") String itemId) {
+        orderService.orderItem(itemId);
+        return "ok";
+    }
+
+    @GetMapping("/no-log")
+    public String noLog() {
+        return "ok";
+    }
+
+}
