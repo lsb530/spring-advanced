12543891605e682e51a35288eaad374bda705ce5
boki <lsb530@naver.com>
2024-10-29 15:47:23 +0900

    JDK 동적 프록시 - 적용1

--- proxy/src/main/java/com/boki/proxy/ProxyApplication.java
+++ proxy/src/main/java/com/boki/proxy/ProxyApplication.java
@@ -4,6 +4,7 @@ import com.boki.proxy.config.AppV1Config;
 import com.boki.proxy.config.AppV2Config;
 import com.boki.proxy.config.v1_proxy.ConcreteProxyConfig;
 import com.boki.proxy.config.v1_proxy.InterfaceProxyConfig;
+import com.boki.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
 import com.boki.proxy.trace.logtrace.LogTrace;
 import com.boki.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -14,7 +15,8 @@ import org.springframework.context.annotation.Import;
 //@Import({AppV1Config.class, AppV2Config.class})
 // config 패키지 하위의 Configuration들이 컴포넌트 스캔에 잡히지 않게 하기 위함
 //@Import(InterfaceProxyConfig.class)
-@Import(ConcreteProxyConfig.class)
+//@Import(ConcreteProxyConfig.class)
+@Import(DynamicProxyBasicConfig.class)
 @SpringBootApplication(scanBasePackages = "com.boki.proxy.app.v3")
 public class ProxyApplication {
 
