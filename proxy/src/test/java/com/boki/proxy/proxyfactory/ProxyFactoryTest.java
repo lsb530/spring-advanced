b631b2ac0e12d954ba7f93960d797bf015742865
boki <lsb530@naver.com>
2024-10-29 17:47:08 +0900

    프록시 팩토리 - 예제 코드2

--- proxy/src/test/java/com/boki/proxy/proxyfactory/ProxyFactoryTest.java
+++ proxy/src/test/java/com/boki/proxy/proxyfactory/ProxyFactoryTest.java
@@ -1,6 +1,7 @@
 package com.boki.proxy.proxyfactory;
 
 import com.boki.proxy.common.advice.TimeAdvice;
+import com.boki.proxy.common.service.ConcreteService;
 import com.boki.proxy.common.service.ServiceImpl;
 import com.boki.proxy.common.service.ServiceInterface;
 import lombok.extern.slf4j.Slf4j;
@@ -30,5 +31,43 @@ public class ProxyFactoryTest {
         assertThat(AopUtils.isAopProxy(proxy)).isTrue();
         assertThat(AopUtils.isJdkDynamicProxy(proxy)).isTrue();
         assertThat(AopUtils.isCglibProxy(proxy)).isFalse();
+    }
+
+    @DisplayName("구체 클래스만 있으면 CGLIB 프록시 사용")
+    @Test
+    void concreteProxy() {
+        ConcreteService target = new ConcreteService();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.addAdvice(new TimeAdvice());
+        ConcreteService proxy = (ConcreteService) proxyFactory.getProxy();
+        log.info("target.getClass()={}", target.getClass());
+        log.info("proxy.getClass()={}", proxy.getClass());
+
+        proxy.call();
+
+        // ProxyFactory를 통해 만들어진 경우에만 검증 가능
+        assertThat(AopUtils.isAopProxy(proxy)).isTrue();
+        assertThat(AopUtils.isJdkDynamicProxy(proxy)).isFalse();
+        assertThat(AopUtils.isCglibProxy(proxy)).isTrue();
+    }
+
+    @DisplayName("ProxyTargetClass 옵션을 사용하면 인터페이스가 있어도 CGLIB를 사용하고, 클래스 기반 프록시 사용")
+    @Test
+    void proxyTargetClass() {
+        ServiceImpl target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        // 타겟 클래스를 기반으로 프록시를 만드는 옵션
+        proxyFactory.setProxyTargetClass(true);
+        proxyFactory.addAdvice(new TimeAdvice());
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+        log.info("target.getClass()={}", target.getClass());
+        log.info("proxy.getClass()={}", proxy.getClass());
+
+        proxy.save();
+
+        // ProxyFactory를 통해 만들어진 경우에만 검증 가능
+        assertThat(AopUtils.isAopProxy(proxy)).isTrue();
+        assertThat(AopUtils.isJdkDynamicProxy(proxy)).isFalse();
+        assertThat(AopUtils.isCglibProxy(proxy)).isTrue();
     }
 }
