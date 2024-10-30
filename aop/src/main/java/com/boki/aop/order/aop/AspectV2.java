fd58fedaca3d79a0dec3bf73ce8c0342feb97601
boki <lsb530@naver.com>
2024-10-30 12:16:33 +0900

    스프링 AOP 구현2 - 포인트컷 분리

--- aop/src/main/java/com/boki/aop/order/aop/AspectV2.java
+++ aop/src/main/java/com/boki/aop/order/aop/AspectV2.java
@@ -4,15 +4,17 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
+import org.aspectj.lang.annotation.Pointcut;
 
 @Slf4j
 @Aspect
-public class AspectV1 {
+public class AspectV2 {
 
     // com.boki.aop.order 패키지 & 하위 패키지
-    @Around("execution(* com.boki.aop.order..*(..))")
+    @Pointcut("execution(* com.boki.aop.order..*(..))")
+    private void allOrder() { } // pointcut signature
+
+    @Around("allOrder()")
     public Object doLog(ProceedingJoinPoint joinPoint) throws Throwable {
         log.info("[log] {}", joinPoint.getSignature()); // join point 시그니처
         return joinPoint.proceed();
--- aop/src/test/java/com/boki/aop/AopTest.java
+++ aop/src/test/java/com/boki/aop/AopTest.java
@@ -3,6 +3,7 @@ package com.boki.aop;
 import com.boki.aop.order.OrderRepository;
 import com.boki.aop.order.OrderService;
 import com.boki.aop.order.aop.AspectV1;
+import com.boki.aop.order.aop.AspectV2;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -12,7 +13,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
 @Slf4j
-@Import(AspectV1.class)
+//@Import(AspectV1.class)
+@Import(AspectV2.class)
 @SpringBootTest
 public class AopTest {
 
