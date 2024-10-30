bc6a6c345ab5561a8bb4ae129045520009b0cf74
boki <lsb530@naver.com>
2024-10-30 18:32:06 +0900

    execution - 2

--- aop/src/test/java/com/boki/aop/pointcut/ExecutionTest.java
+++ aop/src/test/java/com/boki/aop/pointcut/ExecutionTest.java
@@ -4,6 +4,7 @@ import com.boki.aop.member.MemberServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.aspectj.AspectJExpressionPointcut;
 
@@ -93,6 +94,75 @@ public class ExecutionTest {
     @Test
     void packageMatchSubPackage2() {
         pointcut.setExpression("execution(* com.boki.aop..*.*(..))");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    /* Type Match */
+
+    @Test
+    void typeExactMatch() {
+        pointcut.setExpression("execution(* com.boki.aop.member.MemberServiceImpl.*(..))");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    @DisplayName("자식 -> 부모 (타입 매칭)")
+    @Test
+    void typeMatchSuperType() {
+        pointcut.setExpression("execution(* com.boki.aop.member.MemberService.*(..))");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    @DisplayName("자식타입 있는 메서드는 허용")
+    @Test
+    void typeMatchInternal() throws NoSuchMethodException {
+        pointcut.setExpression("execution(* com.boki.aop.member.MemberServiceImpl.*(..))");
+
+        Method internalMethod = MemberServiceImpl.class.getMethod("internal", String.class);
+        assertThat(pointcut.matches(internalMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    @DisplayName("부모타입에 있는 메서드만 허용")
+    @Test
+    void typeMatchNoSuperTypeMethodFalse() throws NoSuchMethodException {
+        pointcut.setExpression("execution(* com.boki.aop.member.MemberService.*(..))");
+
+        Method internalMethod = MemberServiceImpl.class.getMethod("internal", String.class);
+        assertThat(pointcut.matches(internalMethod, MemberServiceImpl.class)).isFalse();
+    }
+
+    /* Parameter Match */
+    @DisplayName("String 타입의 파라미터 허용: (String)")
+    @Test
+    void argsMatch() {
+        pointcut.setExpression("execution(* *(String))");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    @DisplayName("파라미터가 없어야 함: ()")
+    @Test
+    void argsMatchNoArgs() {
+        pointcut.setExpression("execution(* *())");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isFalse();
+    }
+
+    @DisplayName("정확히 하나의 파라미터 허용, 모든 타입 허용: (Xxx)")
+    @Test
+    void argsMatchStar() {
+        pointcut.setExpression("execution(* *(*))");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    @DisplayName("숫자와 무관하게 모든 파라미터, 모든 타입 허용: (), (Xxx), (Xxx, Xxx)")
+    @Test
+    void argsMatchAll() {
+        pointcut.setExpression("execution(* *(..))");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    @DisplayName("String 타입으로 시작하고 숫자와 무관하게 모든 파라미터, 모든 타입 허용: (String), (String, Xxx), (String, Xxx, Xxx)")
+    @Test
+    void argsMatchComplex() {
+        pointcut.setExpression("execution(* *(String, ..))");
         assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
     }
 
